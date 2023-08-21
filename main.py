54901207a3a132214ec613fd20e1860206a57458
= <=>
2023-08-21 12:43:15 -0300

    1

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 from tkinter import *
 
-#Janela
 root = Tk()
-root.title("Calculadora de Macros")
-root.geometry("500x500")
 
-#Textos
-pesoLabel = Label(root, text="Peso : ", font = "Calibri 24 bold")
-pesoLabel.grid(row=0, column=0)
+class Application():
+    def __init__(self):
+        self.root = root
+        self.tela()
+        self.labelsTela()
+        self.entrysTela()
+        self.botoesTela()
+        root.mainloop()
+    def tela(self):
+        self.root.title("Calculadora de Macros")
+        self.root.geometry("500x500")
+        self.root.resizable(False, False)
+        self.root.configure(background="#F18A85")
+    def labelsTela(self):
+        pesoLabel = Label(root, text="Peso : ", font="Calibri 24 bold", background="#F18A85")
+        pesoLabel.grid(row=0, column=0)
+    def entrysTela(self):
+        pesoEntry = Entry(root)
+        pesoEntry.grid(row=0, column=1)
+    def botoesTela(self):
+        calculoBotao = Button(root, text="Calcular")
+        calculoBotao.grid(row=1, column=0)
 
-#Entrada
-pesoEntry = Entry(root)
-pesoEntry.grid(row=0, column=1)
-calculoBotao = Button(root, text="Calcular")
-calculoBotao.grid(row=1, column=0)
-#Loop Principal
-root.mainloop()
+Application()
